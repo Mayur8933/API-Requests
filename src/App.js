a8f267755e8efcd93af7f5cec4c6b974aa78bd12
Mayur Patil <62591248+Mayur8933@users.noreply.github.com>
2022-09-28 22:05:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import "./styles.css";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function App() {
   const [apiFetch, setApiFetch] = useState(null);
+  const [ifError, setIfError] = useState(null);
+
+  //API request using axios
+  const getAPIData = async () => {
+    //for error handling we've used try/catch block here
+    try {
+      const response = await axios.get(
+        "https://api.github.com/users/hadley/orgs"
+      );
+      setApiFetch(response.data);
+    } catch (error) {
+      setIfError(error.message);
+    }
+  };
 
   useEffect(() => {
-    // fetch using promise
-    fetch("https://api.github.com/users/hadley/orgs")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("data --> ", data);
-        setApiFetch(data);
-      })
-      .catch((err) => console.log("err --> ", err));
+    // fetch()
+    //we're using then and catch like Promise, because fetch() will always return a 'Promise'
+    // fetch("https://api.github.com/users/hadley/orgs")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log("data --> ", data);
+    //     setApiFetch(data);
+    //   })
+    //   .catch((err) => console.log("err --> ", err));
+
+    getAPIData();
   }, []);
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-
+      <h1>API Requests</h1>
+      <h2> -- Result -- </h2>
+      <div>{!apiFetch && ifError && <h2>{ifError}</h2>}</div>
       <div>
         {apiFetch &&
           apiFetch.map((item) => {
